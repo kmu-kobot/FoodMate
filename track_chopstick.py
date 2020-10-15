ae2d4460c64134b0e40606ce9e615ed01e568931
Hambbuk <hanbin1931@gmail.com>
2020-10-16 00:10:13 +0900

    Update track_chopstick.py
    Co-Authored-By: hambbuk <51123268+hambbuk@users.noreply.github.com>

--- track_chopstick.py
+++ track_chopstick.py
@@ -1,205 +1,47 @@
 import cv2 as cv
 import numpy as np
-import os
+
+"""
+def make_mask_image(img_bgr):
+    img_hsv = cv.cvtColor(img_bgr, cv.COLOR_BGR2HSV)
+
+    # img_h,img_s,img_v = cv.split(img_hsv)
+
+    low = (0, 30, 0)
+    high = (15, 255, 255)
+
+    img_mask = cv.inRange(img_hsv, low, high)
+    img_mask = cv.bitwise_not(img_mask)
+    return img_mask
+"""
+
+def detect_chopstic(img, thr, minLineLength, maxLineGap):
+    edges = cv.Canny(img, 50, 150, apertureSize = 3)
+    # lines = cv.HoughLines(edges, 1,np.pi/180,thr)
+    lines = cv.HoughLinesP(edges, 1,np.pi/180,thr, minLineLength=minLineLength,maxLineGap=maxLineGap)
+    if lines is None:
+        return None,None,None,None
+    """
+    for line in lines:
+        r, theta = line[0]
+        length = 100
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*r
+        y0 = b*r
+        x1 = int(x0 + length*(-b))
+        y1 = int(y0 + length*a)
+        x2 = int(x0 - length*(-b))
+        y2 = int(y0 - length*a)
+    """
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+    return x1,y1,x2,y2
 
 
-def detect(img, cascade):
-    rects = cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),
-                                     flags=cv.CASCADE_SCALE_IMAGE)
-    if len(rects) == 0:
-        return []
-    rects[:, 2:] += rects[:, :2]
-    return rects
+cap = cv.VideoCapture(0)
 
-
-def removeFaceAra(img, cascade):
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    gray = cv.equalizeHist(gray)
-    rect = detect(gray, cascade)
-
-    return rect
-
-
-def findMaxArea(contours):
-    max_contour = None
-    max_area = -1
-
-    for contour in contours:
-        area = cv.contourArea(contour)
-
-        x, y, w, h = cv.boundingRect(contour)
-
-        if (w * h) * 0.4 > area:
-            continue
-
-        if w > h:
-            continue
-
-        if area > max_area:
-            max_area = area
-            max_contour = contour
-
-    if max_area < 10000:
-        max_area = -1
-
-    return max_area, max_contour
-
-
-def distanceBetweenTwoPoints(start, end):
-    x1, y1 = start
-    x2, y2 = end
-
-    return int(np.sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2)))
-
-
-def calculateAngle(A, B):
-    A_norm = np.linalg.norm(A)
-    B_norm = np.linalg.norm(B)
-    C = np.dot(A, B)
-
-    angle = np.arccos(C / (A_norm * B_norm)) * 180 / np.pi
-    return angle
-
-
-def getFingerPosition(max_contour, img_result, debug):
-    points1 = []
-
-    # STEP 6-1
-    M = cv.moments(max_contour)
-
-    cx = int(M['m10'] / M['m00'])
-    cy = int(M['m01'] / M['m00'])
-
-    max_contour = cv.approxPolyDP(max_contour, 0.02 * cv.arcLength(max_contour, True), True)
-    hull = cv.convexHull(max_contour)
-
-    for point in hull:
-        if cy > point[0][1]:
-            points1.append(tuple(point[0]))
-
-    if debug:
-        cv.drawContours(img_result, [hull], 0, (0, 255, 0), 2)
-        for point in points1:
-            cv.circle(img_result, tuple(point), 15, [0, 0, 0], -1)
-
-    # STEP 6-2
-    hull = cv.convexHull(max_contour, returnPoints=False)
-    defects = cv.convexityDefects(max_contour, hull)
-
-    if defects is None:
-        return -1, None
-
-    points2 = []
-    for i in range(defects.shape[0]):
-        s, e, f, d = defects[i, 0]
-        start = tuple(max_contour[s][0])
-        end = tuple(max_contour[e][0])
-        far = tuple(max_contour[f][0])
-
-        angle = calculateAngle(np.array(start) - np.array(far), np.array(end) - np.array(far))
-
-        if angle < 90:
-            if start[1] < cy:
-                points2.append(start)
-
-            if end[1] < cy:
-                points2.append(end)
-
-    if debug:
-        cv.drawContours(img_result, [max_contour], 0, (255, 0, 255), 2)
-        for point in points2:
-            cv.circle(img_result, tuple(point), 20, [0, 255, 0], 5)
-
-    # STEP 6-3
-    points = points1 + points2
-    points = list(set(points))
-
-    # STEP 6-4
-    new_points = []
-    for p0 in points:
-
-        i = -1
-        for index, c0 in enumerate(max_contour):
-            c0 = tuple(c0[0])
-
-            if p0 == c0 or distanceBetweenTwoPoints(p0, c0) < 20:
-                i = index
-                break
-
-        if i >= 0:
-            pre = i - 1
-            if pre < 0:
-                pre = max_contour[len(max_contour) - 1][0]
-            else:
-                pre = max_contour[i - 1][0]
-
-            next = i + 1
-            if next > len(max_contour) - 1:
-                next = max_contour[0][0]
-            else:
-                next = max_contour[i + 1][0]
-
-            if isinstance(pre, np.ndarray):
-                pre = tuple(pre.tolist())
-            if isinstance(next, np.ndarray):
-                next = tuple(next.tolist())
-
-            angle = calculateAngle(np.array(pre) - np.array(p0), np.array(next) - np.array(p0))
-
-            if angle < 90:
-                new_points.append(p0)
-
-    return 1, new_points
-
-
-def process(img_bgr, img_binary, debug):
-    img_result = img_bgr.copy()
-
-    # # STEP 1
-    # img_bgr = removeFaceAra(img_bgr, cascade)
-
-    # # STEP 2
-    # img_binary = make_mask_image(img_bgr)
-
-    # # STEP 3
-    # kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
-    # img_binary = cv.morphologyEx(img_binary, cv.MORPH_CLOSE, kernel, 1)
-    # if debug:
-    #   cv.imshow("Binary", img_binary)
-
-    # STEP 4
-    contours, hierarchy = cv.findContours(img_binary, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-
-    if debug:
-        for cnt in contours:
-            cv.drawContours(img_result, [cnt], 0, (255, 0, 0), 3)
-
-            # STEP 5
-    max_area, max_contour = findMaxArea(contours)
-
-    if max_area == -1:
-        return img_result
-
-    if debug:
-        cv.drawContours(img_result, [max_contour], 0, (0, 0, 255), 3)
-
-        # STEP 6
-    ret, points = getFingerPosition(max_contour, img_result, debug)
-
-    # STEP 7
-    if ret > 0 and len(points) > 0:
-        for point in points:
-            cv.circle(img_result, point, 20, [255, 0, 255], 5)
-
-    return img_result
-
-
-current_file_path = os.path.dirname(os.path.realpath(__file__))
-cascade = cv.CascadeClassifier(cv.samples.findFile(current_file_path + "\haarcascade_frontalface_alt.xml"))
-cap = cv.VideoCapture('hand.avi')
-
-#  http://layer0.authentise.com/segment-background-using-computer-vision.html
-fgbg = cv.createBackgroundSubtractorMOG2(varThreshold=200, detectShadows=0)
+fgbg = cv.createBackgroundSubtractorMOG2(varThreshold=300, detectShadows=0)
 
 index = 0
 
@@ -208,26 +50,33 @@ while (1):
 
     ret, frame = cap.read()
     if ret == False:
-        break;
+        break
 
     frame = cv.flip(frame, 1)
+    # 살색 제거 마스크 이미지
+    # mask_img = make_mask_image(frame)
 
+    # 차영상 검출
     blur = cv.GaussianBlur(frame, (5, 5), 0)
-    rect = removeFaceAra(frame, cascade)
-
     fgmask = fgbg.apply(blur, learningRate=0)
-
     kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
     fgmask = cv.morphologyEx(fgmask, cv.MORPH_CLOSE, kernel, 2)
-
-    height, width = frame.shape[:2]
-    for x1, y1, x2, y2 in rect:
-        cv.rectangle(fgmask, (x1 - 10, 0), (x2 + 10, height), (0, 0, 0), -1)
-
-    img_result = process(frame, fgmask, debug=False)
-
-    cv.imshow('mask', fgmask)
-    cv.imshow('result', img_result)
+    #########################################
+    # x1,y1,x2,y2 = detect_chopstic(fgmask, 80)
+    # fgmask = cv.bitwise_and(mask_img, fgmask)
+    # fgmask = cv.medianBlur(fgmask,9)
+    # cv.imshow('mask', mask_img)
+    ###########################################
+    # 젓가락 끝점 좌표
+    x1,y1,x2,y2 = detect_chopstic(fgmask, 90,20,100)
+    # 직선이 검출되면 선분과 끝점 그리기
+    if x1 != None:
+        cv.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+        cv.circle(frame, (x1, y1), 10, (255, 0, 0), -1)
+    # 원본 이미지
+    cv.imshow('img', frame)
+    # 차영상
+    cv.imshow('result', fgmask)
 
     key = cv.waitKey(30) & 0xff
     if key == 27:
