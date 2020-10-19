26a27afcd7f165724096a048c09882c7e0a47388
Hambbuk <hanbin1931@gmail.com>
2020-10-19 18:21:51 +0900

    숟가락 detecting 추가
    숟가락 detecting 추가
    손가락을 잡는 문제 있음

--- track_chopstick.cpp
+++ track_chopstick.cpp
@@ -20,9 +20,6 @@ Vec4i detect_chopstic(Mat img, int thr, int minLineLength, int maxLineGap)
 	vector<Vec4i> lines;
 	HoughLinesP(img_canny, lines, 1, CV_PI / 180, thr, minLineLength, maxLineGap);
 
-	Mat img_hough;
-	img.copyTo(img_hough);
-
 	if (lines.empty())
 	{
 		return NULL;
@@ -30,6 +27,23 @@ Vec4i detect_chopstic(Mat img, int thr, int minLineLength, int maxLineGap)
 
 	return lines[0];
 }
+
+Vec3f detect_spoon(Mat img, int minDistance, int thr)
+{
+	Mat img_canny;
+	Canny(img, img_canny, 50, 150);
+	imshow("canny", img_canny);
+	vector<Vec3f> circles;
+	HoughCircles(img_canny, circles, HOUGH_GRADIENT, 1, minDistance, thr, thr / 2, 10, 50);
+
+	if (circles.empty())
+	{
+		return NULL;
+	}
+
+	return circles[0];
+}
+
 
 int main(int, char**)
 {
@@ -42,7 +56,7 @@ int main(int, char**)
 	int deviceID = 0;             // 0 = open default camera
 	int apiID = cv::CAP_ANY;      // 0 = autodetect default API
 								  // open selected camera using selected API
-	Vec4i l = NULL;
+
 	cap.open(deviceID + apiID);
 	cap.set(CAP_PROP_AUTOFOCUS, 0);
 	// check if we succeeded
@@ -74,14 +88,23 @@ int main(int, char**)
 		///////////////////////////////////////////////////////////////
 		Mat kernel = getStructuringElement(MORPH_RECT, Size(5, 5), Point(-1, -1));
 		morphologyEx(foregroundMask, foregroundMask, MORPH_CLOSE, kernel);
+		Vec4i l = NULL;
 		l = detect_chopstic(foregroundMask, 80, 15, 100);
 		// show live and wait for a key with timeout long enough to show images
 		if (l[0] > 0 && l[1] > 0 && l[2] > 0 && l[3] > 0)
 		{
 			line(frame, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 255, 0), 2, 10);
 			//draw dot at upper point
-			int x,y = l[1] > l[3] ? l[2] : l[0], l[1] > l[3] ? l[3] : l[1]
-			circle(frame,x,y), 5, Scalar(255, 0, 0), -1);
+			int x = l[1] > l[3] ? l[2] : l[0];
+			int y = l[1] > l[3] ? l[3] : l[1];
+			circle(frame, Point(x, y), 5, Scalar(255, 0, 0), -1);
+		}
+		Vec3f c = NULL;
+		//detect spoon
+		c = detect_spoon(foregroundMask, 100, 30);
+		if (c[0] > 0 && c[1] > 0 && c[2] > 0)
+		{
+			circle(frame, Point(c[0], c[1]), c[2], Scalar(0, 255, 0), 2);
 		}
 		imshow("Live", frame);
 		imshow("mask", foregroundMask);
