c730e379a0b41a23b3ce1a1cf1dc982f447c952a
Hambbuk <hanbin1931@gmail.com>
2020-10-25 07:46:41 +0900

    은수저, 나무수저 검출 안됨

--- track_chopstick.cpp
+++ track_chopstick.cpp
@@ -1,142 +1,130 @@
-/**
-@file videocapture_basic.cpp
-@brief A very basic sample for using VideoCapture and VideoWriter
-@author PkLab.net
-@date Aug 24, 2016
-*/
-
 #include <opencv2/opencv.hpp>
 #include <iostream>
-#include <stdio.h>
 
 using namespace cv;
 using namespace std;
 
-
-
-Vec4i detect_chopstic(Mat img, int thr, int minLineLength, int maxLineGap)
-{
-	Mat img_canny;
-	Canny(img, img_canny, 50, 150);
-
+class Tracking {
+private:
+	Mat blur, skin_mask, foreground_mask, img_canny;
+	//차영상 background 설정(history = -1, threshold = 400, shadow = false) 
+	Ptr<BackgroundSubtractor> bg_model = createBackgroundSubtractorMOG2(-1, 400, false);
 	vector<Vec4i> lines;
-	HoughLinesP(img_canny, lines, 1, CV_PI / 180, thr, minLineLength, maxLineGap);
-
-	if (lines.empty())
-	{
-		return NULL;
-	}
-
-	return lines[0];
-}
-
-Vec3f detect_spoon(Mat img, int minDistance, int thr)
-{
-	Mat img_canny;
-	Canny(img, img_canny, 50, 150);
-	imshow("canny", img_canny);
 	vector<Vec3f> circles;
-	HoughCircles(img_canny, circles, HOUGH_GRADIENT, 1, minDistance, thr, thr / 2, 10, 50);
-
-	if (circles.empty())
-	{
-		return NULL;
-	}
-
-	return circles[0];
-}
-
-Mat make_mask_image(Mat frame)
-{
 	Mat img_YCrCb;
 	Mat mask;
-	cvtColor(frame, img_YCrCb, COLOR_BGR2YCrCb);
-	inRange(img_YCrCb, Scalar(0, 133, 77), Scalar(255, 173, 127), mask);
-	//vector<Mat> planes;
-	//split(img_YCrCb, planes);
-	//Mat mask = (128 < planes[1]) & (planes[1] < 170) & (73 < planes[2]) & (planes[2] < 158);
+public:
 
-	bitwise_not(mask, mask);
-	return mask;
-}
 
-int main(int, char**)
-{
-	Mat frame, blur, mask_img, foregroundMask;
-	//--- INITIALIZE VIDEOCAPTURE
-	VideoCapture cap;
-	// open the default camera using default API
-	cap.open(0);
-	// OR advance usage: select any API backend
-	int deviceID = 0;             // 0 = open default camera
-	int apiID = cv::CAP_ANY;      // 0 = autodetect default API
-								  // open selected camera using selected API
-
-	cap.open(deviceID + apiID);
-	cap.set(CAP_PROP_AUTOFOCUS, 0);
-	// check if we succeeded
-	if (!cap.isOpened()) {
-		cerr << "ERROR! Unable to open camera\n";
-		return -1;
-	}
-	Ptr<BackgroundSubtractor> bg_model = createBackgroundSubtractorMOG2(0, 300, false);
-
-	//--- GRAB AND WRITE LOOP
-	cout << "Start grabbing" << endl;
-	for (;;)
+	//젓가락 좌표 반환하는 함수
+	Vec4i detect_chopstic(Mat img, int thr, int minLineLength, int maxLineGap)
 	{
-		// wait for a new frame from camera and store it into 'frame'
-		cap.read(frame);
+		lines.clear();
+		HoughLinesP(img, lines, 1, CV_PI / 180, thr, minLineLength, maxLineGap);
+		if (lines.empty())
+		{
+			return NULL;
+		}
+
+		return lines[0];
+	}
+	//숟가락 좌표 반환하는 함수
+	Vec3f detect_spoon(Mat img, int minDistance, int thr)
+	{
+		circles.clear();
+		HoughCircles(img, circles, HOUGH_GRADIENT, 1, minDistance, thr, thr / 2, 10, 50);
+		if (circles.empty())
+		{
+			return NULL;
+		}
+
+		return circles[0];
+	}
+	//살색 지우는 함수
+	Mat make_mask_image(Mat img)
+	{
+		cvtColor(img, img_YCrCb, COLOR_BGR2YCrCb);
+		inRange(img_YCrCb, Scalar(0, 133, 77), Scalar(255, 173, 127), mask);
+		bitwise_not(mask, mask);
+		return mask;
+	}
+	//tracking point
+	Point2i tracking_point(Mat frame)
+	{
+		if (frame.empty())
+		{
+			cout << "Could not open camera :(" << endl;
+		}
+
+		//frame 영상 전처리(640x480, 좌우반전, 3x3 가우시안 블러)
 		resize(frame, frame, Size(640, 480));
 		flip(frame, frame, 1);
-		GaussianBlur(frame, blur, Size(5, 5), 0);
-		// check if we succeeded
-		if (frame.empty()) {
-			cerr << "ERROR! blank frame grabbed\n";
-			break;
-		}
-		// create foreground mask of proper size
-		if (foregroundMask.empty()) {
-			foregroundMask.create(frame.size(), frame.type());
+		GaussianBlur(frame, blur, Size(3, 3), 0);
+
+		//초기 차영상 마스크 Mat 생성
+		if (foreground_mask.empty())
+		{
+			foreground_mask.create(frame.size(), frame.type());
 		}
 
-
-		mask_img = make_mask_image(frame);
-		imshow("mask_img", mask_img);
-		bg_model->apply(blur, foregroundMask, false);
-		///////////////////////////////////////////////////////////////
+		// 1.차영상 마스크 씌우고 노이즈 제거(5x5 미디언 필터)
+		bg_model->apply(blur, foreground_mask);
 		Mat kernel = getStructuringElement(MORPH_RECT, Size(5, 5), Point(-1, -1));
-		morphologyEx(foregroundMask, foregroundMask, MORPH_CLOSE, kernel);
+		morphologyEx(foreground_mask, foreground_mask, MORPH_CLOSE, kernel);
+		medianBlur(foreground_mask, foreground_mask, 5);
 
+		// 2.차영상에서 살색제거 후 skin_mask에 저장
+		copyTo(frame, skin_mask, foreground_mask);
+		skin_mask = make_mask_image(frame);
 
-		bitwise_and(mask_img, foregroundMask, foregroundMask);
-		medianBlur(foregroundMask, foregroundMask, 9);
-		imshow("mask", foregroundMask);
+		// 3.식기만 남은 영상 이진화
+		bitwise_and(skin_mask, foreground_mask, foreground_mask);
 
-		//detect chopstick
-		Vec4i l = NULL;
-		l = detect_chopstic(foregroundMask, 70, 15, 100);
-		// show live and wait for a key with timeout long enough to show images
-		if (l[0] > 0 && l[1] > 0 && l[2] > 0 && l[3] > 0)
+		// 4.이진화 영상에 Canny 적용
+		Canny(foreground_mask, img_canny, 50, 150);
+		//디버깅용
+		imshow("canny_img", img_canny);
+
+		// 5.숟가락을 찾으면 바로 반환하고, 숟가락이 없으면 젓가락 좌표 반환
+		//숟가락 포인트
+		Vec3f s = NULL;
+		s = detect_spoon(img_canny, 10, 30);
+		if (s[0] > 0 && s[1] > 0 && s[2] > 0)
 		{
-			line(frame, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 255, 0), 2, 10);
-			//draw dot at upper point
-			int x = l[1] > l[3] ? l[2] : l[0];
-			int y = l[1] > l[3] ? l[3] : l[1];
-			circle(frame, Point(x, y), 5, Scalar(255, 0, 0), -1);
-		}
-		//detect spoon
-
-		Vec3f c = NULL;
-		c = detect_spoon(foregroundMask, 10, 30);
-		if (c[0] > 0 && c[1] > 0 && c[2] > 0)
-		{
-			circle(frame, Point(c[0], c[1]), c[2], Scalar(0, 255, 0), 2);
-			circle(frame, Point(c[0], c[1]), 2, Scalar(255, 0, 0), -1);
+			circle(frame, Point(s[0], s[1]), s[2], Scalar(0, 255, 0), 2);
+			//circle(frame, Point(s[0], s[1]), 2, Scalar(255, 0, 0), -1);
+			return Point(s[0], s[1]);
 		}
 
-		imshow("Live", frame);
-		imshow("mask", foregroundMask);
+		//젓가락 포인트
+		Vec4i c = NULL;
+		c = detect_chopstic(img_canny, 70, 15, 100);
+		if (c[0] > 0 && c[1] > 0 && c[2] > 0 && c[3] > 0)
+		{
+			line(frame, Point(c[0], c[1]), Point(c[2], c[3]), Scalar(0, 255, 0), 2, 10);
+			//영상에서 더 위에있는 포인트 반환
+			return Point(c[1] > c[3] ? c[2] : c[0], c[1] > c[3] ? c[3] : c[1]);
+			//circle(frame, Point(x, y), 5, Scalar(255, 0, 0), -1);
+		}
+	}
+};
+
+int main()
+{
+	Mat frame;
+	Point2i x, y;
+	VideoCapture cap;
+	Tracking t = Tracking();
+	cap.open(0);
+	cap.set(CAP_PROP_AUTOFOCUS, 0);
+
+	while (true)
+	{
+		cap.read(frame);
+		flip(frame, frame, 1);
+		x, y = t.tracking_point(frame);
+		circle(frame, (x, y), 5, Scalar(255, 0, 0), -1);
+		imshow("frame", frame);
 		if (waitKey(5) >= 0)
 			break;
 	}
