cc20a79bc8e9ebf87edbd30b4cb9acb9bf879a26
sun3850 <sun3850@naver.com>
2020-10-26 01:43:15 +0900

    Update Board.h

--- src/image_Mgr/Board.h
+++ src/image_Mgr/Board.h
@@ -1,21 +1,28 @@
 #pragma
-#include "opencv2/opencv.hpp"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
-#include <iostream>
-#include <string>
 
 
-class Board {
-private:
-    int thresh = 10;
-    Point center_board; // 식판의 중심 좌표 (center_board.x, center_board.y)
-
-public:
-    Board();
-
-    cv::Mat img_preproces(cv::Mat src);
-    cv::Mat get_target_area(cv::Mat src); // 식판 영역
-    std::vector<cv::Mat> frgm_board(cv::Mat src); // 식판내의 구분된 영역들 모음
+struct board_obj{
+    cv::Point center_board;
+    cv::Mat crop_imgs;
 };
+
+struct frgm_obj{
+    std::vector<cv::Mat> crop_imgs; // 잘린 이미지를 담는곳
+    std::vector<cv::Rect> crop_Rects; // 잘린 이미지의 위치를 담는곳 
+};
+
+
+class Board{
+    private:
+        int thresh = 10;
+
+
+    public:
+        
+        Board();
+
+        cv::Mat img_preproces(cv::Mat src);
+        board_obj get_target_area(cv::Mat src);
+        frgm_obj frgm_board(cv::Mat src);
+};
+
