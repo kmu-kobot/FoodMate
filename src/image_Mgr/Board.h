360a1357daa665551b3f96cc0b018ceee207c49d
dodo486 <47061005+Johyeyoung@users.noreply.github.com>
2020-10-26 00:33:53 +0900

    Update Board.h

--- src/image_Mgr/Board.h
+++ src/image_Mgr/Board.h
@@ -1,16 +1,21 @@
 #pragma
+#include "opencv2/opencv.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <iostream>
+#include <string>
 
 
+class Board {
+private:
+    int thresh = 10;
+    Point center_board; // 식판의 중심 좌표 (center_board.x, center_board.y)
 
-class Board{
-    private:
-        int thresh = 10;
+public:
+    Board();
 
-    public:
-        Board();
-
-        cv::Mat img_preproces(cv::Mat src);
-        cv::Mat get_target_area(cv::Mat src);
-        std::vector<cv::Mat> thresh_callback(cv::Mat src);
+    cv::Mat img_preproces(cv::Mat src);
+    cv::Mat get_target_area(cv::Mat src); // 식판 영역
+    std::vector<cv::Mat> frgm_board(cv::Mat src); // 식판내의 구분된 영역들 모음
 };
-
