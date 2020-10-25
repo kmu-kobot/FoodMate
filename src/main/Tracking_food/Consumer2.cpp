c406b0e95331dacaf0a50dd1c431df2c9ed78450
dodo486 <47061005+Johyeyoung@users.noreply.github.com>
2020-10-26 08:07:25 +0900

    Update Consumer2.cpp

--- src/main/Tracking_food/Consumer2.cpp
+++ src/main/Tracking_food/Consumer2.cpp
@@ -1,13 +1,15 @@
 #include "Tracking.h"  
 #include "Matching.h"
 #include "Consumer2.h"
-
+#include "Sound.h"
+#include <iostream>
 using namespace cv;
+using namespace std;
 
 //.......... 필요한 객체들 생성
 Tracking tracker = Tracking();  // 젓가락 또는 숫가락을 pointing 
-Matching matcher = Matching();  // 영역에 해당하는 음식이름 반환
-
+Matching matching = Matching();  // 영역에 해당하는 음식이름 반환
+Sound sound = Sound(); // 음성 안내 
 
 
 
@@ -21,11 +23,13 @@ void* Consumer2::consumer_doing(const Mat& frame, vector<pair<string, Rect> >& r
     Point center = tracker.tracking_point(frame);
 
     // .......2. 구한 좌표가 특정 음식 영역 어디에 있나 확인한다.
-    string  answer = matcher.match_food(center, result);
-
+    string  answer = matching.match_food(center, result);
 
     // .......3. 나온 결과를 음성으로 안내한다.
-
-
+    sound.play_sound(answer);
+    cout << "결과: " << answer << endl;
 
 }
+
+
+
