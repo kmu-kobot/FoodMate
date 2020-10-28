1427850677c3226558cc1fe4265f339493999f28
otnaj <20171623sw@kookmin.ac.kr>
2020-10-28 20:19:06 +0900

    update server

--- src/main/Tracking_food/server_main.py
+++ src/main/Tracking_food/server_main.py
@@ -20,7 +20,7 @@ class Main:
 
         # .......... 1. Server  TCP 소켓 열고 수신 대기
         TCP_IP = 'localhost'
-        TCP_PORT = 10008
+        TCP_PORT = 1119
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((TCP_IP, TCP_PORT))
         self.server.listen(1)
@@ -35,33 +35,45 @@ class Main:
 
         # 2.... Parent process
         else:
+        # .......... 2. Client 접속 성공 & data 받기
+            conn, addr = self.server.accept()
+            print("접속에 성공했습니다")
             limit = 0
             result = [] # 결과값을 담는 리스트
             while True:  # server 의 while
-                # .......... 2. Client 접속 성공 & data 받기
-                conn, addr = self.server.accept()
+                
                 length = self.recvall(conn, 16)
-                print("들어오는 데이터", length)
-                if length.d:   # .......... 3. 이미지 데이터,  받은 이미지를 저장한다.
-                    stringData = self.recvall(conn, int(length))
+                
+                if length:
+                   
 
-                    data = numpy.fromstring(stringData, dtype='uint8')
-                    decimg = cv2.imdecode(data, 1)
-                    print("hehehehe")
-                    # 3. keras로 음식 맞추기
-                    answer = self.guess.realGuess(decimg)  # 일단 급식판에 무슨 음식이 있는지 학습
-                    result.append(answer)
+                    if limit == 0:   # .......... 3. 이미지 데이터,  받은 이미지를 저장한다.
+                        print("들어오는 데이터", length)
+                        mark = length.split(b'\n')[0]
+                        print(mark)
+                        print("들어올 이미지수입니다", mark)
+                        limit = mark  # 들어올 총 이미지의 개수를 update
+                    else:
+                        stringData = self.recvall(conn, int(length))
+                        data = numpy.fromstring(stringData, dtype='uint8')
+                        decimg = cv2.imdecode(data, 1)
+                        print(stringData)
+                        cv2.imshow('ffff',decimg)
+                        cv2.waitKey(0)
+                        # 3. keras로 음식 맞추기
+                        answer = self.guess.realGuess(decimg)  # 일단 급식판에 무슨 음식이 있는지 학습
+                        result.append(answer)
+
+
+
+
+                    if len(result) == limit:
+                        # 4. 도출된 답 전송하기
+                        result = "\n\b\n\b".join(result)
+                        conn.send(result.encode('utf-8'))
+                        result = []  # 다음 사진을 위해 비우기
                 else:
-                   # 숫자의 경우
-                    mark = length.split(b'\n\b\n\b')[0].decode()
-                    limit = int(mark)  # 들어올 총 이미지의 개수를 update
-                    print("들어올 이미지수입니다", mark)
-
-                if len(result) == limit:
-                    # 4. 도출된 답 전송하기
-                    result = "\n\b\n\b".join(result)
-                    conn.send(result.encode('utf-8'))
-                    result = []  # 다음 사진을 위해 비우기
+                    print("이미지가 업슷ㅂ니다")
             exit(0)
 
     # socket 수신 버퍼를 읽어서 반환하는 함수
@@ -74,11 +86,7 @@ class Main:
             count -= len(newbuf)
         return buf
 
-    def connect_C_client(self):
-        pass
-        img = None
-        return img
-
+   
 if __name__ == '__main__':
     main = Main()
     main.make_child_process()
