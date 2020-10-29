bf783b68d6063e78a404465661c6f3f16a6e78d7
dodo486 <47061005+Johyeyoung@users.noreply.github.com>
2020-10-29 18:34:02 +0900

    change image into plt

--- src/main/Tracking_food/guess.py
+++ src/main/Tracking_food/guess.py
@@ -6,28 +6,28 @@ from PIL import Image, ImageOps
 import numpy as np
 import os
 
+
 class Guess:
     def __init__(self):
         # Disable scientific notation for clarity
         np.set_printoptions(suppress=True)
-        
+
         # Load the model
         self.model = tensorflow.keras.models.load_model('../data/model/keras_model.h5')
-
         self.data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-
 
     # 해당 path 에 있는 이미지들을 labeling 한다.
     def realGuess(self, decimg):
-
         # 음식리스트 가져오기
-        label = ['계란말이', '김치', '된장국', '멸치조림', '시금치','우엉조림','흰밥']
+        label = ['계란말이', '김치', '된장국', '멸치조림', '시금치', '우엉조림', '흰밥']
 
         # Replace this with the path to your image
+        # 이미지를 plt 형태로 바꿔야됨 
+        color_coverted = cv2.cvtColor(decimg, cv2.COLOR_BGR2RGB)
+        decimg = Image.fromarray(color_coverted)
         size = (224, 224)
         image = ImageOps.fit(decimg, size, Image.ANTIALIAS)
-
-        #turn the image into a numpy array
+        # turn the image into a numpy array
         image_array = np.asarray(image)
         # Normalize the image
         normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
@@ -36,16 +36,13 @@ class Guess:
         # run the inference
         prediction = self.model.predict(self.data)
         print(prediction[0])  # 세부 예측빈도
-        max_pct = np.max(prediction[0]) # max_percentage
-        max_pct_idx = np.argmax(prediction[0]) # max_percentage's index
+        max_pct = np.max(prediction[0])  # max_percentage
+        max_pct_idx = np.argmax(prediction[0])  # max_percentage's index
         answer = label[max_pct_idx]
 
         # 디버깅용 코드
         print("▶ " + answer)
-        cv2.imshow("input", decimg)
-        cv2.waitKey(0)
         return answer
-
 
 
 if __name__ == '__main__':
