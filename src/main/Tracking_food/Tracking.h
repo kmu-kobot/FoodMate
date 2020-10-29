0015d8dc3cdff5cca92970254d4234a1a2d177db
dodo486 <47061005+Johyeyoung@users.noreply.github.com>
2020-10-29 18:13:46 +0900

    Traking -> Traker

--- src/main/Tracking_food/Tracking.h
+++ src/main/Tracking_food/Tracking.h
@@ -6,7 +6,7 @@ using namespace std;
 
 
 
-class Tracking {
+class Tracker {
 private:
 	Mat blur, skin_mask, foreground_mask, img_canny;
 	//차영상 background 설정(history = -1, threshold = 400, shadow = false) 
@@ -15,19 +15,15 @@ private:
 	vector<Vec3f> circles;
 	Mat img_YCrCb;
 	Mat mask;
-public:
 
+public:
 
 	//젓가락 좌표 반환하는 함수
 	Vec4i detect_chopstic(Mat img, int thr, int minLineLength, int maxLineGap);
-
 	//숟가락 좌표 반환하는 함수
 	Vec3f detect_spoon(Mat img, int minDistance, int thr);
-
 	//살색 지우는 함수
 	Mat make_mask_image(Mat img);
-
 	//tracking point 젓가락의 좌표 반환 _______ 이것만 사용!!
-	Point2i tracking_point(Mat frame);
+	Point2i track_point(Mat frame);
 };
-
