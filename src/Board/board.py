5810b546cfea034e2f0447c07ce1ad5764d89631
sun3850 <sun3850@naver.com>
2020-11-02 02:46:53 +0900

    Add files via upload

---
+++ src/Board/board.py
@@ -0,0 +1,67 @@
+# write by kyoung chip , jang
+#
+# python 3.6
+#
+#
+from ctypes import *
+import numpy as np
+import cv2
+ 
+class Board(object):
+ 
+    def __init__(self):
+ 
+        self.board_lib = cdll.LoadLibrary('./libBoard.so')
+        self.obj = self.board_lib.Board_new()
+ 
+    def img_preproces(self, src):
+
+        cv2.imshow("test2", self.board_lib.board_img_preproces( self.obj, src ))
+ 
+    def get_target_area( self) :
+        self.board_lib.board_get_target_area(self.obj)
+   
+    def board_frgm_board(self):
+        
+        self.board_lib.board_frgm_board(self.obj)
+    
+    def board_rectArea(self, rect):
+
+        self.board_lib.board_rectArea(self.obj, rect)
+
+
+    def get_center_board(self):
+        center_board_x = self.board_lib.center_board_x(self.obj)
+        center_board_y = self.board_lib.center_board_y(self.obj)
+        point = [center_board_x, center_board_y]
+        return point
+
+
+    def get_crop_Rects(self):
+        idex = self.board_lib.crop_Rects_size()
+        print(idex)
+        rects = []
+        for i in range(idex):
+            x = self.board_lib.crop_Rects_x(self.obj,i)
+            y = self.board_lib.crop_Rects_y(self.obj,i)
+            w = self.board_lib.crop_Rects_y(self.obj,i)
+            h = self.board_lib.crop_Rects_h(self.obj,i)
+            rect = [x, y, w, h]
+            rects.append(rect)
+
+        return rects
+ 
+class PT_data(Structure) : 
+    _fields_ = [("x", c_int), ("y", c_int), ("w", c_int), ("h", c_int)] 
+
+if __name__ == '__main__':
+ 
+    b = Board()
+    b.get_target_area()
+    b.board_frgm_board()
+    rects = b.get_crop_Rects()
+    print(rects)
+    
+    
+
+
