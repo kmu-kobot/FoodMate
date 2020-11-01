0249314a76aefab45293171b3b16701b85df3dd9
sun3850 <sun3850@naver.com>
2020-11-02 02:42:47 +0900

    Update and rename src/test_src/Board.h to src/Board/include/Board.h

--- src/Board/include/Board.h
+++ src/Board/include/Board.h
@@ -11,22 +11,31 @@ struct frgm_obj{
     std::vector<cv::Rect> crop_Rects; // 잘린 이미지의 위치를 담는곳 
 };
 
+typedef struct{
+    int x = 5;
+    int y = 1;
+    int w = 2;
+    int h = 3;
+}PT_data;
+
 
 class Board{
     private:
         int thresh = 25;
-        board_obj my_board_obj;
-        frgm_obj my_frgm_obj;
-
 
     public:
-        
-        Board();
 
+        Board();
         cv::Mat img_preproces(cv::Mat src);
         void get_target_area();
         void frgm_board();
         float rectArea(cv::Rect rect);
-        cv::Rect rect_return(); 
-        cv::Point point_return();
+        int get_crop_Rects_size();
+        int get_center_board_x();
+        int get_center_board_y();
+        int get_crop_Rects_x(int idex);
+        int get_crop_Rects_y(int idex);
+        int get_crop_Rects_w(int idex);
+        int get_crop_Rects_h(int idex);
+
 };
