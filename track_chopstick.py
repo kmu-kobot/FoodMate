c1f1f46df075dd6f9b4589d40e4bfee2387ffaaa
Hambbuk <51123268+Hambbuk@users.noreply.github.com>
2020-11-03 02:00:09 +0900

    hh

--- track_chopstick.py
+++ track_chopstick.py
@@ -16,24 +16,14 @@ def make_mask_image(img_bgr):
     return img_mask
 
 def detect_chopstic(img, thr, minLineLength, maxLineGap):
+    cur_line=[]
     edges = cv.Canny(img, 50, 150, apertureSize=3)
+
     # lines = cv.HoughLines(edges, 1,np.pi/180,thr)
     lines = cv.HoughLinesP(edges, 1, np.pi / 180, thr, minLineLength=minLineLength, maxLineGap=maxLineGap)
+
     if lines is None:
         return None, None, None, None
-    """
-    for line in lines:
-        r, theta = line[0]
-        length = 100
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a*r
-        y0 = b*r
-        x1 = int(x0 + length*(-b))
-        y1 = int(y0 + length*a)
-        x2 = int(x0 - length*(-b))
-        y2 = int(y0 - length*a)
-    """
 
     x1, y1, x2, y2 = lines[0][0]
     return x1, y1, x2, y2
@@ -50,13 +40,13 @@ def detect_spoon(img):
     return x, y, r
 
 
-cap = cv.VideoCapture(0)
-
+cap = cv.VideoCapture(-1)
+ret = cap.set(3, 640)
+ret = cap.set(4, 480)
 fgbg = cv.createBackgroundSubtractorMOG2(varThreshold=300, detectShadows=0)
 
 prevTime = 0
 while (1):
-
     ret, frame = cap.read()
     if ret == False:
         break
@@ -70,7 +60,7 @@ while (1):
     frame = cv.flip(frame, 1)
     # 살색 제거 마스크 이미지
     mask_img = make_mask_image(frame)
-
+    cv.imshow("test",mask_img)
     # 차영상 검출
     blur = cv.GaussianBlur(frame, (5, 5), 0)
     fgmask = fgbg.apply(blur, learningRate=0)
@@ -84,15 +74,15 @@ while (1):
 
     # 젓가락 끝점 좌표
     x1, y1, x2, y2 = detect_chopstic(fgmask, 90, 15, 100)
-    c_x, c_y, r = detect_spoon(fgmask)
+    #c_x, c_y, r = detect_spoon(fgmask)
     # 직선이 검출되면 선분과 끝점 그리기
     if x1 != None:
         cv.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
         cv.circle(frame, (x1, y1), 10, (255, 0, 0), -1)
     # 원이 검출되면 원 그리기
-    if c_x != None:
-        cv.circle(frame, (c_x, c_y), r, (0, 255, 0), 5)
-        cv.circle(frame, (c_x, c_y), 10, (255, 0, 0), -1)
+    # if c_x != None:
+    #     cv.circle(frame, (c_x, c_y), r, (0, 255, 0), 5)
+    #     cv.circle(frame, (c_x, c_y), 10, (255, 0, 0), -1)
     # 원본 이미지
     cv.putText(frame, str, (0, 100), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
     cv.imshow('img', frame)
