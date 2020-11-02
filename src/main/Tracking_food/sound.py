cbf4e5c86815e97825e3b635b2575d15ffea3ae9
new <55116793+otnaj@users.noreply.github.com>
2020-11-02 19:28:00 +0900

    Update sound.py

--- src/main/Tracking_food/sound.py
+++ src/main/Tracking_food/sound.py
@@ -1,11 +1,19 @@
-# !/usr/local/bin/python3
-# -*- coding:utf-8 -*-
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
 import os
+import multiprocessing
+
 
 # "play sound in cmd" not using playsound module
 class Sound:
-    def __init__(self):
-        pass
+    def __init__(self,answer):
+
+        # proc = multiprocessing.current_process()
+        # print(proc.name)
+        # print(proc.pid)
+        p = multiprocessing.Process(name="SubProcess", target=self.play_sound, args=(answer,))
+        p.start()
+
 
     def play_sound(self, dish_name):
         if dish_name == "none": # 아무 것도 들어오지 않은 경우
@@ -19,10 +27,10 @@ class Sound:
             os.system("canberra-gtk-play -f ~/Tracking-Food/data/sound/스캔완료.wav")
             return
         else: # 반찬 이름이 들어울 경우
-            os.system("canberra-gtk-play -f ~/Tracking-Food/data/sound/" + dish_name + ".wav")
+            os.system("canberra-gtk-play -f ~/tracking_food/Tracking-Food/data/sound/" + str(dish_name) + ".wav")
             return
+        
 
 if __name__ == '__main__':
-    test = Sound()
-    test.play_sound("none")
-    test.play_sound("계란말이")
+    test = Sound("계란말이")
+
